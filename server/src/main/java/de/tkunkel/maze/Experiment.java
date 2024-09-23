ee048409cbccaa4ae9ca363360d0b1d702ad45c2
Torsten Kunkel <git@tkunkel.de>
2024-09-23 23:42:56 +0200

    * change to java 21

--- server/src/main/java/de/tkunkel/maze/Experiment.java
+++ server/src/main/java/de/tkunkel/maze/Experiment.java
@@ -15,12 +15,9 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.TimeUnit;
+import java.util.Date;
 
 public class Experiment {
-    private final ExecutorService executorService = new ForkJoinPool(Runtime.getRuntime().availableProcessors() * 2);
     private final Logger LOG = LoggerFactory.getLogger(Experiment.class);
 
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -32,32 +29,15 @@ public class Experiment {
         MazeGenerator mazeGenerator = new MazeGenerator();
         RectangleGenerator generator = new RectangleGenerator(mazeGenerator);
         LOG.debug("Generating Maze...");
-        int size = 50;
+        int size = 500;
         Maze maze = generator.generate(new Location(1, 1), new Location((int) (Math.random() * size), (int) (Math.random() * size)), size, size);
         int sizeOfCell = 10;
-        int widthOfWall = 2;
-        BufferedImage mazeImageWithoutSolution = new RenderImage().render(maze, sizeOfCell, widthOfWall);
 
-        LOG.debug("Solving Maze...");
-        new DijkstraSolver().solve(maze);
-        int maxDistanceFromStart = getMaxDistance(maze);
 
         LOG.debug("Queing rending...");
-        for (int distanceFromStartToDraw = 0; distanceFromStartToDraw < maxDistanceFromStart; distanceFromStartToDraw++) {
-            int finalDistanceFromStartToDraw = distanceFromStartToDraw;
-            executorService.submit(() -> {
-                try {
-                    LOG.debug("Rendering solution step %d of %d".formatted(finalDistanceFromStartToDraw, maxDistanceFromStart));
-                    BufferedImage mazeImageWithSolution = new RenderSolvingProcess().renderSolutionProgress(mazeImageWithoutSolution, maze, sizeOfCell, finalDistanceFromStartToDraw, maxDistanceFromStart);
-                    ImageIO.write(mazeImageWithSolution, "PNG", new File("/tmp/maze_%05d.png".formatted(finalDistanceFromStartToDraw)));
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-        }
+        BufferedImage mazeImageWithSolution = new RenderImage().render( maze, sizeOfCell,2);
+        ImageIO.write(mazeImageWithSolution, "PNG", new File("/tmp/maze_%05d.png".formatted(new Date().getTime())));
         LOG.debug("Waiting to finish...");
-        executorService.shutdown();
-        executorService.awaitTermination(10, TimeUnit.MINUTES);
         LOG.debug("Done...");
     }
 
